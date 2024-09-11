b40aa675a2955631b4b1c97ced6be0ee94f026bb
bened_wards <ben.cayde.edwards@gmail.com>
2024-09-11 11:52:32 +1000

    single wheel test

--- motor_control/loops.cpp
+++ motor_control/loops.cpp
@@ -11,20 +11,20 @@ namespace {
 namespace Loops {
     // infinitely loop waiting for rising edge on encoder A pin
     // on rising edge ticks the encoder
-    void encoder_event_handler(Encoder& encoder, bool debug=false) {
+    void encoder_event_handler(Encoder& encoder) {
         std::shared_ptr<gpiod::line> encALine = encoder.getEncALine();
         std::shared_ptr<gpiod::line> encBLine = encoder.getEncBLine();
         while (!stopFlag.load()) {
             bool eventA = encALine->event_wait(std::chrono::microseconds(1));
             if (eventA) {
                 bool rising = encALine->event_read().event_type == gpiod::line_event::RISING_EDGE;
-                if (debug) std::cout << "Event A. Rising: " << (rising) << std::endl;
+                std::cout << "Event A. Rising: " << (rising) << std::endl;
                 encoder.tick(true, rising);
             }
             bool eventB = encBLine->event_wait(std::chrono::microseconds(1));
             if (eventB) {
                 bool rising = encBLine->event_read().event_type == gpiod::line_event::RISING_EDGE;
-                if (debug) std::cout << "Event B. Rising: " << (rising) << std::endl;
+                std::cout << "Event B. Rising: " << (rising) << std::endl;
                 encoder.tick(false, rising);
             }
         }
--- tests/single_wheel_test.cpp
+++ tests/single_wheel_test.cpp
@@ -6,7 +6,7 @@
 #include "config.h"
 #include "loops.h"
 
-void poll(std::chrono::seconds duration, Wheel& leftWheel, Wheel& rightWheel, int speedInterruptMillis) {
+void poll(std::chrono::seconds duration, Wheel& leftWheel, int speedInterruptMillis) {
     std::cout << "Running main polling loop in main thread for " << static_cast<long>(duration.count()) << "seconds\n";
     auto start = std::chrono::steady_clock::now();
     auto end = start + duration;
@@ -17,11 +17,9 @@ void poll(std::chrono::seconds duration, Wheel& leftWheel, Wheel& rightWheel, in
             std::this_thread::sleep_for(std::chrono::microseconds(100));
         }
         double leftSpeed = leftWheel.getSpeed();
-        double rightSpeed = rightWheel.getSpeed();
         printCounter++;
-        if (printCounter > 10) {
+        if (printCounter > 1000000000) {
             std::cout << "Left wheel speed: " << leftSpeed << "rad/s. RPM: " << leftSpeed * 60 / (2 * M_PI) << std::endl;
-            std::cout << "Right wheel speed: " << rightSpeed << "rad/s. RPM: " << rightSpeed * 60 / (2 * M_PI) << std::endl;
             printCounter = 0;
         }
     }
@@ -36,58 +34,42 @@ int main(int argc, char** argv)
     std::cout << "Creating motors\n";
     std::cout << "Creating left motor\n";
     Motor leftMotor = factory.createMotor(config.enableLeft, config.in1Left, config.in2Left);
-    std::cout << "Creating right motor\n";
-    Motor rightMotor = factory.createMotor(config.enableRight, config.in1Right, config.in2Right);
 
     std::cout << "Creating encoders\n";
     std::cout << "Creating left encoder\n";
     Encoder leftEncoder = factory.createLeftEncoder(config.encALeft, config.encBLeft, config.ticksPerRev, config.speedInterruptMillis);
-    std::cout << "Creating right encoder\n";
-    Encoder rightEncoder = factory.createRightEncoder(config.encARight, config.encBRight, config.ticksPerRev, config.speedInterruptMillis);
 
     std::cout << "Creating wheels (needs motors and encoders)";
     Wheel leftWheel = factory.createWheel(config.wheelRadius, leftMotor, leftEncoder);
-    Wheel rightWheel = factory.createWheel(config.wheelRadius, rightMotor, rightEncoder);
 
     try {
         std::cout << "Creating left encoder thread\n";
         std::thread leftThread(Loops::encoder_event_handler, std::ref(leftEncoder));
-        std::cout << "Creating right encoder thread\n";
-        std::thread rightThread(Loops::encoder_event_handler, std::ref(rightEncoder));
 
         std::cout << "BOTH tests\n";
         std::cout << "Driving forwards for 5s\n";
         leftMotor.sendCommand(0.5, Direction::FORWARD);
-        rightMotor.sendCommand(0.5, Direction::FORWARD);
-        poll(std::chrono::seconds(5), leftWheel, rightWheel, config.speedInterruptMillis);
+        poll(std::chrono::seconds(5), leftWheel, config.speedInterruptMillis);
         leftMotor.stop();
-        rightMotor.stop();
 
         std::cout << "Turning left\n";
         leftMotor.sendCommand(0.5, Direction::FORWARD);
-        rightMotor.sendCommand(0.5, Direction::REVERSE);
-        poll(std::chrono::seconds(5), leftWheel, rightWheel, config.speedInterruptMillis);
+        poll(std::chrono::seconds(5), leftWheel, config.speedInterruptMillis);
         leftMotor.stop();
-        rightMotor.stop();
 
         std::cout << "Turning right\n";
         leftMotor.sendCommand(0.5, Direction::REVERSE);
-        rightMotor.sendCommand(0.5, Direction::FORWARD);
-        poll(std::chrono::seconds(5), leftWheel, rightWheel, config.speedInterruptMillis);
+        poll(std::chrono::seconds(5), leftWheel, config.speedInterruptMillis);
         leftMotor.stop();
-        rightMotor.stop();
 
         std::cout << "Driving backwards\n";
         leftMotor.sendCommand(0.5, Direction::REVERSE);
-        rightMotor.sendCommand(0.5, Direction::REVERSE);
-        poll(std::chrono::seconds(5), leftWheel, rightWheel, config.speedInterruptMillis);
+        poll(std::chrono::seconds(5), leftWheel, config.speedInterruptMillis);
         leftMotor.stop();
-        rightMotor.stop();
         
         // kill encoder loops
         Loops::stopFlag = true;
         leftThread.join();
-        rightThread.join();
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
